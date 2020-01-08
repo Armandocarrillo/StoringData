efb3c408f1c241289ec1e3d680b5808625e9e1b1
Armandocarrillo <ingcompuarmando@gmail.com>
2020-01-08 13:33:16 -0600

    Storing data

--- storingData WatchKit Extension/InterfaceController.swift
+++ storingData WatchKit Extension/InterfaceController.swift
@@ -8,15 +8,27 @@
 
 import WatchKit
 import Foundation
-
+let defaults = UserDefaults.standard
 
 class InterfaceController: WKInterfaceController {
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
-    }
+        testUserDefaults()//escribe datos del usuario porque no encontro una cadena
+        testUserDefaults()// lee datos del ususario que nosotros ingresamos
+        }
+    
+    func testUserDefaults() {
+    // get hold of our app's user defaults let defaults = UserDefaults.standard
+    if let contents = defaults.string(forKey: "shared_default") { //unwrap para recuperar una string de "shared_default"
+          // saved data was found!
+    print("Reading from defaults") //imprime en consola esta leyenda
+          print(contents)// imprime la caadena que encontro
+       } else { //si no exixte una cadena
+    // no saved data – create it!
+    print("Writing to defaults")//imprime en consola
+    defaults.set("This is the saved default.", forKey: "shared_default") //con metodo set recupera CUALQUIER dato
+    } }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
